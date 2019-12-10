45c7246674c1fa59b59e6bff53c2418790e9eab0
Sergii Butenko <sergii.butenko@globallogic.com>
2019-12-10 14:03:05 +0200

    updated slides

--- lectures/lecture9.python.http/slide33.py
+++ lectures/lecture9.python.http/slide33.py
@@ -1,23 +1,8 @@
 import requests
-from requests.auth import HTTPBasicAuth
 
 response = requests.post(
     'http://localhost:5002/login',
     json={'username': 'test', 'password': 'test'}
     )
 
-token = response.json()['access_token']
-print(f"token: {token}\n")
-
-print("sending not authoraized request")
-response = requests.get(
-    'http://localhost:5002/protected',
-    )
-print(response.text)    
-
-print("sending authoraized request")
-response = requests.get(
-    'http://localhost:5002/protected',
-    headers={'Authorization': f"Bearer {token}"}
-    )
-print(response.text)    
+print(response.json())
