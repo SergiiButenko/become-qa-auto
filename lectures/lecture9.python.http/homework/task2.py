e4c9ae2b3cb1d62a8c87ed8c909e27e18d705f9a
Yuriy Klimchuk <yuriy.klimchuk@globallogic.com>
2019-12-12 15:25:19 +0200

    some changes

--- lectures/lecture9.python.http/homework/task2.py
+++ lectures/lecture9.python.http/homework/task2.py
@@ -3,3 +3,20 @@
 # 2. Создать ресурс item
 # 3. Прочитать ресурс item
 # 4. Удалить ресурс item
+
+import requests
+response = requests.post(
+    'http://localhost:5002/login',
+    json={'username': 'test', 'password': 'test'}
+    )
+
+token = response.json()['access_token']
+print(f"token: {token}\n")
+print(response.text)
+
+print("sending authoraized request to http://localhost:5002")
+response1 = requests.get(
+    'http://localhost:5002/protected',
+    headers={'Authorization': f"Bearer {token}"}
+    )
+print(response1.text)
---
+++ yklimchuk/lecture9.python.http/task1.py
@@ -0,0 +1,21 @@
+# Задание1. Разобрать код. 
+# 1. Ответить на вопрос: "Что тут происходит?"
+# 2 .Что такое raise_for_status()
+# 3. for цикл в питоне
+# 4. как добавить третий запрос на 'http://localhost:5002/basic_auth'
+import requests # импортируем библиотеку requests
+from requests.exceptions import HTTPError # из библиотеки requests.exceptions импортируем HTTPError
+ 
+for url in ['http://localhost:5002/', 'http://localhost:5002/fail500','http://localhost:5002/basic_auth']: # в цикле перебираются урлы
+    try: # обработка исключений. Здесь в блоке мы выполняем инструкцию, которая может породить исключение.
+        print(f"\nSending GET request to {url}") # информационный вывод об отправке реквеста на итерируемый урл
+        response = requests.get(url) # создается переменная с результатом вызова состояния
+
+        response.raise_for_status() # вызов исключения, если запрос был неудачным.
+        # HTTPError будет вызываться для определенных кодов состояния. Если код состояния указывает на успешный запрос, программа продолжит работу без возникновения этого исключения.
+    except HTTPError as http_err:  # обработка исключений. Здесь в блоке мы перехватываем исключения.
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print('Success!')          # если никаких исключений/ ошибок в итерируемой урле не возникло, выводится сообщение об успешном результате.
