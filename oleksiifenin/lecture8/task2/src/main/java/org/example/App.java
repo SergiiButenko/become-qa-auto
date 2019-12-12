a148daa94c1041eeff8717ac9be096d9ff4449bf
Thealeshka <alexfenin1234@gmail.com>
2019-12-12 13:39:06 +0200

    homework

---
+++ oleksiifenin/lecture8/task2/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TaskTwo.getInstance().execute();
+    }
+}
