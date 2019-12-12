cd252a8bf856c948dc97ceee6480595b95022171
Sergii Butenko <sergii.butenko@globallogic.com>
2019-12-12 02:26:36 +0200

    added lecture 9

---
+++ lectures/lecture9.python.selenium/slide34.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+#This example requires Selenium WebDriver 3.13 or newer
+driver = webdriver.Chrome('./linux_chromedriver')
+driver.get("https://google.com/ncr")
+driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
+first_result = driver.find_element_by_css_selector("h3")
+first_result.click()
+first_result = driver.find_element_by_partial_link_text('casein')
+print(first_result.get_attribute("textContent"))
