ad15f539b0e234e39daf131c40497c6f3011d2f5
Olha Kysla <olha.kysla@globallogic.com>
2019-12-19 11:18:00 +0200

    pytest

--- olhkys/seleniumTest/seleniumTest.py
+++ olhkys/seleniumTest/seleniumTest.py
@@ -1,11 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+#This example requires Selenium WebDriver 3.13 or newer
 driver = webdriver.Chrome('./win_chromedriver.exe')
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." in driver.page_source
-# driver.close()
+driver.get("https://google.com/ncr")
+driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
+first_result = driver.find_element_by_css_selector("h3")
+print(first_result.get_attribute("textContent"))
+viki = driver.find_element_by_tag_name('wikipedia')
+viki.click()
